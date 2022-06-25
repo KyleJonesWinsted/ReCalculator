8748a9d19e5e95552446ca338c56ec60884e0388
Kyle Jones <KyleJonesWinsted@gmail.com>
2022-06-25 09:35:34 -0500

    Initial Commit

---
+++ Shared/ReCalculatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  ReCalculatorApp.swift
+//  Shared
+//
+//  Created by Kyle Jones on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ReCalculatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
