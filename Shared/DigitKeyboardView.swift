6a32500f649a28d1723e435ecfd980ae05f00924
Kyle Jones <KyleJonesWinsted@gmail.com>
2022-07-03 15:40:32 -0500

    Adding creating new variables

--- Shared/DigitKeyboardView.swift
+++ Shared/DigitKeyboardView.swift
@@ -9,29 +9,60 @@ import SwiftUI
 
 
 struct DigitKeyboard: View {
+    @Binding var input: String
+    @State private var info = GridInfo()
+    var addVariable: () -> Void
     let columns = [
         GridItem(.flexible()),
-        GridItem(.flexible()),
+        GridItem(.flexible(), alignment: .leading),
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
     let labels = [
-        "7","8","9","÷",
-        "4","5","6","×",
-        "1","2","3","-",
-        "0",".","AC", "+"
+        "7","8","9","×",
+        "4","5","6","-",
+        "1","2","3","+",
+        "(","0",".", ")"
     ]
-    var onDigit: (String) -> Void
-    
+    let colors: [String: Color] = [
+        "×": .orange,
+        "-": .orange,
+        "+": .orange,
+        "(": .mint,
+        ")": .mint,
+        ".": .mint,
+    ]
     var body: some View {
         LazyVGrid(columns: columns) {
-            ForEach(labels, id: \.self) { digit in
-                DigitButton(label: digit) {
-                    onDigit(digit)
-                }
+            DigitButton(label: "AC", color: .orange) { input.removeAll() }
+            AddVariableButton(width: self.info.columnWidth(1) * 2 + self.info.spacing(1), addVariable: addVariable)
+                .frame(maxWidth: .infinity)
+            Text("")
+            DigitButton(label: "÷", color: .orange) { input.append("÷") }
+            ForEach(0..<labels.count, id: \.self) { idx in
+                let digit = labels[idx]
+                DigitButton(label: digit, color: colors[digit] ?? .gray) { input.append(digit) }
+                    .gridInfoId(idx)
             }
         }
+        .gridInfo($info)
         .padding(10)
+    }
+}
+
+struct AddVariableButton: View {
+    var width: CGFloat
+    var addVariable: () -> Void
+    var body: some View {
+        Button {
+            addVariable()
+        } label: {
+            Text("+ Add Variable")
+                .font(.title2)
+                .frame(width: width, height: 53)
+        }
+        .frame(width: width)
+        .buttonStyle(DigitButtonStyle(color: .mint))
     }
 }
 
@@ -39,6 +70,7 @@ struct DigitKeyboard: View {
 struct DigitButton: View {
     
     var label: String
+    var color = Color.gray
     
     var onClick: () -> Void
     
@@ -48,22 +80,111 @@ struct DigitButton: View {
         } label: {
             Text(label)
                 .font(.largeTitle)
-                .frame( maxWidth: .infinity)
+                .frame(maxWidth: .infinity)
                 .padding(7)
         }
-        .buttonStyle(DigitButtonStyle())
+        .buttonStyle(DigitButtonStyle(color: color))
             
     }
 }
 
 struct DigitButtonStyle: ButtonStyle {
+    var color = Color.gray
     func makeBody(configuration: Configuration) -> some View {
         configuration
             .label
             .foregroundColor(.white)
-            .background(configuration.isPressed ? .blue : .gray)
+            .background(configuration.isPressed ? .blue : color)
             .cornerRadius(30)
             .padding(1)
-//            .frame(height: 50)
+    }
+}
+
+struct GridInfoPreference {
+    let id: Int
+    let bounds: Anchor<CGRect>
+}
+
+struct GridPreferenceKey: PreferenceKey {
+    static var defaultValue: [GridInfoPreference] = []
+    
+    static func reduce(value: inout [GridInfoPreference], nextValue: () -> [GridInfoPreference]) {
+        return value.append(contentsOf: nextValue())
+    }
+}
+
+struct GridInfo: Equatable {
+    // A array of all rendered cells's bounds
+    var cells: [Item] = []
+    
+    // a computed property that returns the number of columns
+    var columnCount: Int {
+        guard cells.count > 1 else { return cells.count }
+
+        var k = 1
+
+        for i in 1..<cells.count {
+            if cells[i].bounds.origin.x > cells[i-1].bounds.origin.x {
+                k += 1
+            } else {
+                break
+            }
+        }
+
+        return k
+    }
+    
+    // a computed property that returns the range of cells being rendered
+    var cellRange: ClosedRange<Int>? {
+        guard let lower = cells.first?.id, let upper = cells.last?.id else { return nil }
+        
+        return lower...upper
+    }
+  
+    // returns the width of a rendered cell
+    func cellWidth(_ id: Int) -> CGFloat {
+        columnCount > 0 ? columnWidth(id % columnCount) : 0
+    }
+    
+    // returns the width of a column
+    func columnWidth(_ col: Int) -> CGFloat {
+        columnCount > 0 && col < columnCount ? cells[col].bounds.width : 0
+    }
+    
+    // returns the spacing between columns col and col+1
+    func spacing(_ col: Int) -> CGFloat {
+        guard columnCount > 0 else { return 0 }
+        let left = col < columnCount ? cells[col].bounds.maxX : 0
+        let right = col+1 < columnCount ? cells[col+1].bounds.minX : left
+        
+        return right - left
+    }
+
+    // Do not forget the "Equatable", as it prevent redrawing loops
+    struct Item: Equatable {
+        let id: Int
+        let bounds: CGRect
+    }
+}
+
+extension View {
+    func gridInfoId(_ id: Int) -> some View {
+        self.anchorPreference(key: GridPreferenceKey.self, value: .bounds) {
+                [GridInfoPreference(id: id, bounds: $0)]
+            }
+    }
+    
+    func gridInfo(_ info: Binding<GridInfo>) -> some View {
+        self.backgroundPreferenceValue(GridPreferenceKey.self) { prefs in
+            GeometryReader { proxy -> Color in
+                DispatchQueue.main.async {
+                    info.wrappedValue.cells = prefs.compactMap {
+                      GridInfo.Item(id: $0.id, bounds: proxy[$0.bounds])
+                    }
+                }
+                    
+                return Color.clear
+            }
+        }
     }
 }
--- Shared/FormulaCreationView.swift
+++ Shared/FormulaCreationView.swift
@@ -7,36 +7,90 @@
 
 import SwiftUI
 
+struct Variable: Identifiable {
+    var id = UUID()
+    var name: String
+    var symbol: String
+    var value: String = ""
+}
+
 struct FormulaCreationView: View {
     
     @State var formulaText = String()
+    @State var variables = [Variable]()
+    @State var isAddingVariable = false
+    @FocusState var isVariableFieldFocused: Bool
+    @State var newVariableName = ""
     
     var body: some View {
-        VStack {
-            ScrollView(.horizontal) {
-                    Text(formulaText)
-                        .font(.largeTitle)
-                        .flipsForRightToLeftLayoutDirection(true)
-                        .environment(\.layoutDirection, .rightToLeft)
-            }
-            .flipsForRightToLeftLayoutDirection(true)
-            .environment(\.layoutDirection, .rightToLeft)
-            .padding()
-            Spacer()
-            DigitKeyboard { input in
-                if input == "AC" {
-                    $formulaText.wrappedValue.removeAll()
-                    return
+        if isAddingVariable {
+            TextField("New Variable Name", text: $newVariableName)
+                .onSubmit {
+                    addNewVariable(name: newVariableName)
+                    isAddingVariable = false
+                    newVariableName = ""
                 }
-                $formulaText.wrappedValue.append(input)
+                .disableAutocorrection(true)
+                .font(.title)
+                .submitLabel(.done)
+                .focused($isVariableFieldFocused)
+                .padding()
+        } else {
+            VStack {
+                FormulaTextView(text: formulaText)
+                VariableListView(input: $formulaText, variables: variables)
+                Spacer()
+                DigitKeyboard(input: $formulaText) {
+                    self.isAddingVariable = true
+                    self.isVariableFieldFocused = true
+                }
             }
         }
     }
+    
+    func addNewVariable(name: String) {
+        let subscripts = ["₀", "₁", "₂", "₃", "₄", "₅", "₆", "₇",  "₈",  "₉"]
+        guard let symbol = name.first else { return }
+        let subIndex = variables
+            .filter { $0.name.starts(with: String(symbol)) }
+            .count
+        variables.append(Variable(name: name, symbol: "\(symbol)\(subscripts[subIndex % subscripts.count])" ))
+    }
 }
 
+struct VariableListView: View {
+    @Binding var input: String
+    var variables: [Variable]
+    var body: some View {
+        List(variables) { variable in
+            HStack {
+                DigitButton(label: variable.symbol) {
+                    input.append(variable.symbol)
+                }
+                .frame(width: 80)
+                Text(variable.name)
+                Spacer()
+                Text(variable.value)
+            }
+        }
+        .listStyle(.plain)
+    }
+}
 
-
-
+struct FormulaTextView: View {
+    var text: String
+    var body: some View {
+        ScrollView(.horizontal) {
+            Text(text.count < 1 ? " " : text)
+                .font(.largeTitle)
+                .flipsForRightToLeftLayoutDirection(true)
+                .environment(\.layoutDirection, .rightToLeft)
+        }
+        .flipsForRightToLeftLayoutDirection(true)
+        .environment(\.layoutDirection, .rightToLeft)
+        .padding()
+    }
+}
 
 struct FormulaCreationView_Preview: PreviewProvider {
     static let devices = [
@@ -48,7 +102,10 @@ struct FormulaCreationView_Preview: PreviewProvider {
     static var previews: some View {
         Group {
             ForEach(devices, id: \.self) { device in
-                FormulaCreationView()
+                FormulaCreationView(formulaText: "2+2",variables: [
+                    Variable(name: "Hello", symbol: "H", value: "42"),
+                    Variable(name: "World", symbol: "W", value: "2")
+                ])
                     .previewDevice(PreviewDevice(rawValue: device))
                     .previewDisplayName(device)
             }
